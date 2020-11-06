d36130874ec2669d0466510b00036487aac2711a
Ahmed Elehwany <ahmed.elehwany@mail.mcgill.ca>
2020-11-05 19:24:34 -0500

    Add sample app for auth using react native

---
+++ samples/react-native/auth/authenticator/App.js
@@ -0,0 +1,42 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Amplify, Auth } from "aws-amplify";
+import config from "./aws-exports";
+import { withAuthenticator } from "aws-amplify-react-native";
+import { AuthState, onAuthUIStateChange } from "@aws-amplify/ui-components";
+import logo from './logo.svg';
+
+Amplify.configure({
+  ...config,
+  Analytics: {
+    disabled: true,
+  },
+});
+
+function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World!</Text>
+      <img src={logo} className="App-logo" alt="logo" />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+export default withAuthenticator(App, {
+  // Render a sign out button once logged in
+  includeGreetings: true,
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 50,
+  },
+});
