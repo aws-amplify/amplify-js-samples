4ee71b90f11dfc8a9309baca2681f7f57df721d7
Chris Bonifacio <christopher.bonifacio@gmail.com>
2021-12-10 19:59:03 -0500

    amplify sample project

---
+++ samples/react/auth/authenticator/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "authenticatordf03bc0b": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "AppClientSecret": "string"
+        }
+    }
+}
