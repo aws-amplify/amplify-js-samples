bb4ca1132db65f093e66d4de0f035690a9b80afd
Thadd Trinh <thadtrin@amazon.com>
2021-12-17 13:36:03 -0800

    Publishing geofence-control-map from staging

---
+++ samples/react/geo/geofence-control-map/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,29 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "geofencecontrolmap77dc1a87": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        },
+        "userPoolGroups": {
+            "geofenceAdminGroupRole": "string"
+        }
+    },
+    "geo": {
+        "map51e71ba7": {
+            "Name": "string",
+            "Style": "string",
+            "Region": "string",
+            "Arn": "string"
+        },
+        "geofenceCollection3b002ef5": {
+            "Name": "string",
+            "Region": "string",
+            "Arn": "string"
+        }
+    }
+}
